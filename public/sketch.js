28ec95cbc18ff013ea262b641797705e8e230a8a
Matt Prindible <code@mattprindible.com>
2021-03-11 00:34:56 -0500

    canvas size and colors

--- public/sketch.js
+++ public/sketch.js
@@ -1,21 +1,21 @@
 var socket;
 
 function setup(){
-    createCanvas(640, 480);
-    background(51);
+    createCanvas(windowWidth, windowHeight);
+    background(233, 235, 238);
     socket = io.connect();
     socket.on('mouse', newDrawing);
 }
 
 function newDrawing(data){
     noStroke();
-    fill(255);
+    fill(31, 255, 212);
     ellipse(data.x, data.y, 20, 20);
 }
 
 function mouseDragged(){
     noStroke();
-    fill(255);
+    fill(31, 255, 212);
     ellipse(mouseX, mouseY, 20, 20);
 
     var data = {
